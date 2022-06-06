2d77f0ff724ae5c203e57d1d78dbfad8d16f16ad
vibracode <codevibra@gmail.com>
2022-06-06 18:24:58 +0430

    like last commit

--- methods/script.js
+++ methods/script.js
@@ -1,7 +1,10 @@
 //print
 console.log("any variable or value here");//prints to the console in browser inspedt tool
 
-let name="methods"
+let name="methods";
 alert("file name: "+ name +"\n"+"this file contains all \"methods\" we need");
 
 isNaN("value"); //if value is number: output=true else output=false
+
+prompt("enter your name: "); // get a value from user
+
