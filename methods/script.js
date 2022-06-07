6f7e5eca9815a0535b11de1b1e445d1e3b6b6113
vibracode <codevibra@gmail.com>
2022-06-07 22:32:25 +0430

    string methods added

--- methods/script.js
+++ methods/script.js
@@ -8,3 +8,21 @@ isNaN("value"); //if value is number: output=true else output=false
 
 prompt("enter your name: "); // get a value from user
 
+
+//string methods
+let text = 'alireza is the best';
+let firstIndex = text[0];//0=a 1=l 2=i ... 18=t
+let stringLength= text.length;//19
+
+text.charAt(2);//i
+text.charCodeAt(2);//i=72 asci code
+text.concat(" so true");//alireza is the best so true
+text.trim();//delete all spaces before and after string not between
+text.toLowerCase();//lowercase all string charachters
+text.toUpperCase();//upercase all string charachters
+text.search("is");//return the index of start char of 'is' in string
+text.indexOf("is");//like search
+text.includes("is");//true if 'is' exist in string otherwise false
+
+
+
