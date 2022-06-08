77c875af2f5d9aaed731f21cafd5b8e3ec564e5d
vibracode <codevibra@gmail.com>
2022-06-08 20:00:38 +0430

    string and math methods completed

--- methods/script.js
+++ methods/script.js
@@ -23,6 +23,28 @@ text.toUpperCase();//upercase all string charachters
 text.search("is");//return the index of start char of 'is' in string
 text.indexOf("is");//like search
 text.includes("is");//true if 'is' exist in string otherwise false
+text.slice(8,13);//'is the' number of index, to this index
+text.substr(8,6);//'is the' number of index, this number after the first number
+text.substring(8,13);//'is the' number of index, to this index like slice
+
+//math methods
+Math.PI();//p=3.14...
+2**3;//8 power
+Math.pow(2,3);//8
+Math.sqrt(16);//4
+Math.abs(-10);//10 delets negetive |-12|=12
+Math.min(20,90,56,5,56);// 5 minimum
+Math.max(20,90,56,5,56);// 96 maximum
+
+Math.floor(14.6);// 14
+Math.trunc(14.6);// 14 like floor
+Math.round(12.8);// 13 round the number 12.4=12
+Math.ceil(12.8);// 13 allways round to the top 12.4=13
+Math.random();// a random number between 0 to 1
+Math.random()*10;// a random number between 0 to 10
+
+
+
 
 
 
