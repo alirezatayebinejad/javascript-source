4bd5aff5aecb31ed919d4b248bb928e39887bce8
vibracode <codevibra@gmail.com>
2022-06-14 21:58:42 +0430

    DOM - get elements

---
+++ 6. DOM/script.js
@@ -0,0 +1,9 @@
+// with DOM we can minipulate web page elements
+
+// get elements
+let h1Elem = document.getElementById('title')
+let liElems = document.getElementsByClassName('list-item')
+let listItems = document.getElementsByTagName('li')
+let firstListItem = document.querySelector('#box') 
+let ulItems = document.querySelectorAll('.list-item')
+
