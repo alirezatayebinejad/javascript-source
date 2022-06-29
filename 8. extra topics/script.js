8e665b9244f25e9e242dc443b8bc20f35ebe00e2
vibracode <codevibra@gmail.com>
2022-06-30 00:33:06 +0430

    all local storage

--- 8. extra topics/script.js
+++ 8. extra topics/script.js
@@ -1,4 +1,8 @@
 //local storage
 localStorage.setItem('keyName','value'); //adds to inspect application tab -> local storage
 localStorage.getItem('keyName'); //gets the value
+localStorage.removeItem('keyName') //removes this key in local storage
 localStorage.clear(); //clear all user's browser local storage
+localStorage.setItem('keyName', JSON.stringify(['ali','amin','amir']))//make every other typeofs into string
+JSON.parse(localStorage.getItem('keyName'));//make every string value to its real typeof
+
