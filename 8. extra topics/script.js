9b83c71631e3b9dc0fd71ced5925f245112b6dde
vibracode <codevibra@gmail.com>
2022-06-29 23:41:25 +0430

    local storage

---
+++ 8. extra topics/script.js
@@ -0,0 +1,4 @@
+//local storage
+localStorage.setItem('keyName','value'); //adds to inspect application tab -> local storage
+localStorage.getItem('keyName'); //gets the value
+localStorage.clear(); //clear all user's browser local storage
