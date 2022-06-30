6f9244dc7a6e9e21ad1c6d0fbbdeb135b5f1d6bd
vibracode <codevibra@gmail.com>
2022-06-30 21:58:13 +0430

    try catch and style

--- 8. extra topics/script.js
+++ 8. extra topics/script.js
@@ -6,3 +6,12 @@ localStorage.clear(); //clear all user's browser local storage
 localStorage.setItem('keyName', JSON.stringify(['ali','amin','amir']))//make every other typeofs into string
 JSON.parse(localStorage.getItem('keyName'));//make every string value to its real typeof
 
+//error handling with try catch
+try {
+    //if these codes have error it send the error to err
+    //can send a custom error with "throw 'message' "
+} catch(err){
+    //these codes will execute if codes from try has an error
+} finally{
+    //these codes will execute no matter what
+}
