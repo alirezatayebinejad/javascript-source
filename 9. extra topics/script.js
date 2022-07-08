f6ea12a54944a30bc1e25f582d05a75157e50b8d
vibracode <codevibra@gmail.com>
2022-07-08 18:10:18 +0430

    indexDB

--- 9. extra topics/script.js
+++ 9. extra topics/script.js
@@ -1,11 +1,3 @@
-//local storage
-localStorage.setItem("keyName", "value"); //adds to inspect application tab -> local storage
-localStorage.getItem("keyName"); //gets the value
-localStorage.removeItem("keyName"); //removes this key in local storage
-localStorage.clear(); //clear all user's browser local storage
-localStorage.setItem("keyName", JSON.stringify(["ali", "amin", "amir"])); //make every other typeofs into string
-JSON.parse(localStorage.getItem("keyName")); //make every string value to its real typeof
-
 //error handling with try catch
 try {
   //if these codes have error it sends the error to err
