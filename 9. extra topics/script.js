a825eb6fa6d850b177ba7ceb704a62cf4f90b59c
vibracode <codevibra@gmail.com>
2022-07-11 15:40:21 +0430

    fixing some problemes

--- 9. extra topics/script.js
+++ 9. extra topics/script.js
@@ -8,20 +8,6 @@ try {
   //these codes will execute no matter what
 }
 
-//async vs sync
-//sync
-console.log("1");
-console.log("2");
-console.log("3");
-console.log("4"); //output: 1 2 3 4
-//async
-console.log("1");
-setTimeout(function () {
-  console.log("2"); //executes after 3 seconds
-}, 3000);
-console.log("3");
-console.log("4"); //output: 1 3 4 2
-
 //strict mode
 /*
   write "use strict" in first line of js file 
@@ -35,14 +21,6 @@ console.log("4"); //output: 1 3 4 2
 let number = 700000000;
 let goodNumber = 700_000_000;
 //browser ignores '_' and not working in strings
-
-//RegEx ES6
-let txt = "i am alireza ,web developer";
-let regexCode = /alireza/; //finds 'alireza' pattern in a string
-regexCode = /a.m/; //first letter 'a' second enything third 'm' can be /a..m/ for four letters etc...
-regexCode = /a.+n/; //first 'a' between and letter for any long and last one should be 'n'
-let emailVerification = /.+@.+.com/; // email true pattern
-console.log(regexCode.test(text)); //true or false
 
 //this keyword
 //on html element <li onclick="removeList(this)">li</li> 'this' send the tag element
@@ -95,59 +73,4 @@ let symbol5 = symbol("sth");
 let symbol6 = symbol("sth");
 //symbol5===symbol6 -> true
 
-//promis ES6 - for asycronize programmin and good for preventing tutorial hell with callback functions
-//forexample thing like checking user login from database which takes time we use promises to do that
-let myPromise = new Promise((resolve, reject) => {
-  //if we do the promise we cal resolve and if we dont we call reject
-
-  setTimeout(() => {
-    console.log("Login check !!");
-    if (loginFlag) {
-      resolve();
-    }
-  }, 3000);
-});
-function success() {
-  console.log("success :)");
-}
-function error() {
-  console.log("error :(");
-}
-myPromise.then(success, error);
-//or
-myPromise
-  .then(success)
-  .catch(error)
-  .finally(() => {
-    //this will execute whether promis is resolved or rejected
-    console.log("promise is done.");
-  });
-//another example
-let books = [
-  { id: 1, name: "Bi Shouri", price: 95000 },
-  { id: 2, name: "Bimarefat", price: 56660 },
-  { id: 3, name: "pedar", price: 75000 },
-];
-function addBook(name, price) {
-  let newBook = {
-    id: books.length + 1,
-    name,
-    price,
-  };
-  return new Promise((resolve, reject) => {
-    setTimeout(function () {
-      if (books.push(newBook)) {
-        resolve();
-      } else {
-        reject();
-      }
-    }, 4000);
-  });
-}
-addBook("golestan", 90_000).then(() => {
-  console.log(books);
-});
-//in these examples we used timeout because we dont have an api that takes time right now
-//phases of promise: pending=no resolve and no reject , fullfilled:resolved , rejected: rejects
-//we can have multiple .then and if promise resolved first .then()happens and first .then() returns a value and next then() gets the value from its input
-
+//AJAX - Asynchronous Javascript And XML - to get data from backend without refreshing webpage
