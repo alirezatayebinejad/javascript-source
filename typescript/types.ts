07a42f2d0d761168447df3977b18297c59004c49
vibracode <codevibra@gmail.com>
2022-12-08 14:20:08 +0330

    ts union types

--- typescript/types.ts
+++ typescript/types.ts
@@ -119,3 +119,45 @@ const allUsers: User3[] = [];
 allUsers.push({ name: "", isActive: true });
 
 const MLModels: number[][] = [[255, 255, 255], []];
+
+// union types
+
+let score: number | string = 33;
+score = 44;
+score = "55";
+
+type User5 = {
+	name: string;
+	id: number;
+};
+
+type Admin = {
+	username: string;
+	id: number;
+};
+
+let hitesh: User5 | Admin = { name: "hitesh", id: 334 };
+hitesh = { username: "hc", id: 334 };
+
+// function getDbId(id: number | string){
+//     //making some API calls
+//     console.log(`DB id is: ${id}`);
+
+// }
+getDbId(3);
+getDbId("3");
+
+function getDbId(id: number | string) {
+	if (typeof id === "string") {
+		id.toLowerCase();
+	}
+}
+
+const data: number[] = [1, 2, 3];
+const data2: string[] = ["1", "2", "3"];
+const data3: (string | number | boolean)[] = ["1", "2", 3, true];
+
+let seatAllotment: "aisle" | "middle" | "window";
+
+seatAllotment = "aisle";
+// seatAllotment = "crew" //not accepted
