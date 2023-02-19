8b5ffec119146119b06311ce5f24526ac0d471b3
vibracode <codevibra@gmail.com>
2023-02-19 22:20:56 +0330

    TS main tutorial code done

--- typescript/3.types.ts
+++ typescript/3.types.ts
@@ -265,3 +265,120 @@ class Sellable<T> {
 		this.cart.push(product);
 	}
 }
+
+//type narrowing
+
+function detectType(val: number | string) {
+	if (typeof val === "string") {
+		return val.toLowerCase();
+	}
+	return val + 3;
+}
+
+function provideId(id: string | null) {
+	if (!id) {
+		console.log("Please provide ID");
+		return;
+	}
+	id.toLowerCase();
+}
+
+function printAll(strs: string | string[] | null) {
+	if (strs) {
+		if (typeof strs === "object") {
+			for (const s of strs) {
+				console.log(s);
+			}
+		} else if (typeof strs === "string") {
+			console.log(strs);
+		}
+	}
+}
+
+interface User6 {
+	name: string;
+	email: string;
+}
+
+interface Admin6 {
+	name: string;
+	email: string;
+	isAdmin: boolean;
+}
+
+//the 'in' in narrowing
+function isAdminAccount(account: User6 | Admin6) {
+	if ("isAdmin" in account) {
+		return account.isAdmin;
+	}
+}
+
+//the instanceof and type predictates
+function logValue(x: Date | string) {
+	if (x instanceof Date) {
+		console.log(x.toUTCString());
+	} else {
+		console.log(x.toUpperCase());
+	}
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+	return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+	if (isFish(pet)) {
+		pet;
+		return "fish food";
+	} else {
+		pet;
+		return "bird Food";
+	}
+}
+
+//Discriminated union & Exhaustiveness checking
+
+interface Circle {
+	kind: "circle";
+	radius: number;
+}
+
+interface Square {
+	kind: "square";
+	side: number;
+}
+
+interface Rectangle {
+	kind: "rectangle";
+	length: number;
+	width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+	if (shape.kind === "circle") {
+		return Math.PI * shape.radius ** 2;
+	}
+	//return shape.side * shape.side
+}
+
+function getArea(shape: Shape) {
+	switch (shape.kind) {
+		case "circle":
+			return Math.PI * shape.radius ** 2;
+
+		case "square":
+			return shape.side * shape.side;
+		case "rectangle":
+			return shape.length * shape.width;
+
+		default:
+			//this variable is just for if we added an interface but did not implement it in use case it will show error
+			const _defaultforshape: never = shape;
+			return _defaultforshape;
+	}
+}
